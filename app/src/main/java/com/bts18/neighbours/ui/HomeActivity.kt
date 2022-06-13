916f697c33ef293f3b28f89374a422d8e51d5c9b
Bhaskar Narayan <krbn1999@gmail.com>
2022-06-13 23:01:51 +0530

    feat: Initial commit

--- app/src/main/java/com/bts18/neighbours/ui/HomeActivity.kt
+++ app/src/main/java/com/bts18/neighbours/ui/HomeActivity.kt
@@ -2,11 +2,14 @@ package com.bts18.neighbours.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.bts18.neighbours.R
+import com.bts18.neighbours.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
     }
 }
